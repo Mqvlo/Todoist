f99115ebad0c78c1de84a98097251604dc04f6ec
Mqvlo <esparabasuras@gmail.com>
2023-07-21 00:01:23 -0300

    Converter added

--- app/src/main/java/com/example/todoist/util/TaskRoomDatabase.java
+++ app/src/main/java/com/example/todoist/util/TaskRoomDatabase.java
@@ -4,9 +4,9 @@ import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.room.Database;
-import androidx.room.Insert;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.todoist.data.TaskDao;
@@ -15,9 +15,10 @@ import com.example.todoist.model.Task;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 @Database(entities = {Task.class}, version = 1, exportSchema = false)
+@TypeConverters({Converter.class})
 public abstract class TaskRoomDatabase extends RoomDatabase {
     public static final int NUMBER_OF_THREADS = 4;
-    public static final String DATABASE_NAME = "todoister_database";
+    public static final String DATABASE_NAME = "todoist_database";
     public static volatile TaskRoomDatabase INSTANCE;
     public static final ExecutorService databaseWriterExecutor
             = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
